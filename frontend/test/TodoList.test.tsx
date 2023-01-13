2a03d395860c6005c7f509119606af69f2ec1a28
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-13 15:24:53 -0300

    test: ensure create a todo list with 3 items and 2 done

--- frontend/test/TodoList.test.tsx
+++ frontend/test/TodoList.test.tsx
@@ -9,4 +9,19 @@ describe('TodoList', () => {
 
         expect(todoList.getCompleted()).toBe(0)
     })
+    
+    test("should create a todo List with 3 items and 2 done", () => {
+        const todoList = new TodoList()
+        todoList.addItem('a')
+        todoList.addItem('b')
+        todoList.addItem('c')
+
+        const a = todoList.getItem('a')
+        if (a) todoList.toggleDone(a)
+
+        const b = todoList.getItem('b')
+        if (b) todoList.toggleDone(b)
+
+        expect(todoList.getCompleted()).toBe(67)
+    })
 })
