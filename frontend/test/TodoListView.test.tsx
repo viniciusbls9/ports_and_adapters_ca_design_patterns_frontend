00d2ce4adb38c17afe1a3b9302eb727b43488b36
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-13 11:54:38 -0300

    test: using TodoMemoryGateway

--- frontend/test/TodoListView.test.tsx
+++ frontend/test/TodoListView.test.tsx
@@ -1,9 +1,8 @@
-import { render, act, waitFor } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { HttpClientContext } from "src/context/HttpClientContext";
-import TodoHttpGateway from "src/gateways/TodoHttpGateway";
-import AxiosAdapter from "src/infra/AxiosAdapter";
 import TodoGateway from "src/gateways/TodoGateway"
 import TodoList from "../src/components/TodoList"
+import TodoMemoryGateway from "src/gateways/TodoMemoryGateway";
 
 function makeSut(todoGateway: TodoGateway) {
     return render(
@@ -24,13 +23,11 @@ function sleep(mili: number) {
 describe('TodoListView', () => {
     test('Should test a todo list screen', async () => {
         const sleepFunction = await sleep(100)
-        const httpClientInstance = new AxiosAdapter()
-        const todoGateway = new TodoHttpGateway(httpClientInstance, 'http://localhost:3001')
+        const todoGateway = new TodoMemoryGateway()
         const { container } = makeSut(todoGateway)
         await sleepFunction
 
         const completed = container.getElementsByClassName('completed')
-
         await waitFor(() => {
             expect(completed[0].textContent).toBe('33 %')
         })
