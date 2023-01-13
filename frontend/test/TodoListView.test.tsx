0284d07acdf9554f4c0feafaa680a56c806b5086
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-13 11:14:44 -0300

    test: import http instances

--- frontend/test/TodoListView.test.tsx
+++ frontend/test/TodoListView.test.tsx
@@ -1,8 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render, act, waitFor } from "@testing-library/react";
+import { HttpClientContext } from "src/context/HttpClientContext";
+import TodoHttpGateway from "src/gateways/TodoHttpGateway";
+import AxiosAdapter from "src/infra/AxiosAdapter";
+import TodoGateway from "src/gateways/TodoGateway"
 import TodoList from "../src/components/TodoList"
 
-function sleep(mili: number) {
+function makeSut(todoGateway: TodoGateway) {
+    return render(
+        <HttpClientContext.Provider value={{ todoGateway }}>
+            <TodoList />
+        </HttpClientContext.Provider>
+    )
+}
 
+function sleep(mili: number) {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(true)
@@ -12,11 +23,16 @@ function sleep(mili: number) {
 
 describe('TodoListView', () => {
     test('Should test a todo list screen', async () => {
-        const { container } = render(<TodoList />)
-        await sleep(100)
+        const sleepFunction = await sleep(100)
+        const httpClientInstance = new AxiosAdapter()
+        const todoGateway = new TodoHttpGateway(httpClientInstance, 'http://localhost:3001')
+        const { container } = makeSut(todoGateway)
+        await sleepFunction
 
         const completed = container.getElementsByClassName('completed')
 
-        expect(completed[0].textContent).toBe('33 %')
+        await waitFor(() => {
+            expect(completed[0].textContent).toBe('33 %')
+        })
     })
 })
